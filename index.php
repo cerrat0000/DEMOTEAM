60ccea808f92a5419b873fa6077806d7fee1cf8f
chus-stark <chusgtz250321@gmail.com>
2026-02-06 14:54:18 -0600

    Update index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Documentos</title>
+</head>
+<body>
+    <h1>Bienvenido a la pagina RoomHub</h1>
+    <h2>integrantes</h2>
+    <ul>
+        <li>
+            <a href="./kevin.docx">kevin alexis cerrato mazariegos</a>
+        </li>
+        <li>
+            <a href="./manual.docx">jesus manuel gutierrez diaz</a>
+        </li>
+        <li>
+            <a href="./acacio.docx">jesus acacio perez jimenez</a>
+        </li>
+    </ul>
+</body>
+</html>
